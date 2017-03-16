c264c30b8778fdb2d9b0b741852e8f3b8e80628c
Dave Goddard <dave@goddard.id.au>
2017-03-16 11:35:26 -0400

    wip; working
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- lib/language_pack/rails5.rb
+++ lib/language_pack/rails5.rb
@@ -1,7 +1,6 @@
 require 'securerandom'
 require "language_pack"
 require "language_pack/rails42"
-require "language_pack/shell_helpers"
 
 class LanguagePack::Rails5 < LanguagePack::Rails42
   # @return [Boolean] true if it's a Rails 5.x app
@@ -9,8 +8,8 @@ class LanguagePack::Rails5 < LanguagePack::Rails42
     instrument "rails5.use" do
       rails_version = bundler.gem_version('railties')
       return false unless rails_version
-      is_rails = rails_version >= Gem::Version.new('5.x') &&
-                 rails_version <  Gem::Version.new('6.0.0')
+      is_rails = rails_version >= Gem::Version.new('5.0.0.beta') &&
+                 rails_version <  Gem::Version.new('5.1.0.beta1')
       return is_rails
     end
   end
@@ -30,19 +29,5 @@ class LanguagePack::Rails5 < LanguagePack::Rails42
 
   def install_plugins
     # do not install plugins, do not call super, do not warn
-  end
-
-
-## TODO ; create Rails51 and add to set
-
-  include LanguagePack::ShellHelpers
-  def run_assets_precompile_rake_task
-    instrument "rails5.run_assets_precompile_rake_task" do
-      if File.exists?('bin/yarn') && File.exists?('yarn.lock')
-        run!("./bin/yarn install")
-      end
-
-      super
-    end
   end
 end
