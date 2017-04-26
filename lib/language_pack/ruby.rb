a14c39ecf976da4f3cfb9312e47aee720033bcb4
Keaty Gross <kgross@pivotal.io>
2017-04-26 14:45:15 -0400

    wip
    Signed-off-by: Nikita Rathi <nrathi@pivotal.io>

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -300,7 +300,7 @@ EOF
   def setup_profiled
     instrument 'setup_profiled' do
       set_env_default  "LANG",     "en_US.UTF-8"
-      set_env_override "GEM_PATH", %Q{$HOME/#{slug_vendor_base}:$DEP_DIR/bundler-#{BUNDLER_VERSION}$([[ ! -z "${GEM_PATH:-}" ]] && echo ":$GEM_PATH")}
+      add_to_profiled %{export GEM_PATH="$HOME/#{slug_vendor_base}:$DEP_DIR/bundler-#{BUNDLER_VERSION}$([[ ! -z "${GEM_PATH:-}" ]] && echo ":$GEM_PATH")"}
       set_env_override "PATH",     binstubs_relative_paths.map {|path| "$HOME/#{path}" }.join(":") + ":$PATH"
 
       add_to_profiled set_default_web_concurrency if env("SENSIBLE_DEFAULTS")
